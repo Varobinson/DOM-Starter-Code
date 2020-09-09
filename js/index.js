da25cc5539378616160b924b0fadecc12b96eda2
Vincent Robinson <59773500+Varobinson@users.noreply.github.com>
2020-09-09 12:23:22 -0400

    finished madlib

--- js/index.js
+++ js/index.js
@@ -1,9 +1,36 @@
 const myButton = document.getElementById('clickMe');
+
+const name = document.getElementById('name');
+const name1 = document.getElementById('name1');
+
+const adj1 = document.getElementById('adj1');
+const adjective1 = document.getElementById('adjective1');
+
+const verb = document.getElementById('verb');
+const verb1 = document.getElementById('verb1');
+
+const place = document.getElementById('place');
+const place1 = document.getElementById('place1');
+
+const noun = document.getElementById('noun');
 const noun1 = document.getElementById('noun1');
-const noun1Input = document.getElementById('noun1Input');
+
+const adj2 = document.getElementById('adj2');
+const noun2Input = document.getElementById('noun2Input');
 
 myButton.addEventListener('click', function (event) {
   event.preventDefault();
 
-  noun1.innerHTML = noun1Input.value;
+  name.innerHTML = name1.value;
+
+  adj1.innerHTML = adjective1.value;
+
+  verb.innerHTML = verb1.value;
+
+  place.innerHTML = place1.value;
+
+  noun.innerHTML = noun1.value;
+
+  adj2.innerHTML = adjective2.value;
+
 });
